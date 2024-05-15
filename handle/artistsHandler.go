65fc580bf97f8245a60128361b7e66bf964363d8
3li7alaki <alihalaki@outlook.com>
2024-05-15 19:32:11 +0300

    Geo'd

--- handle/artistsHandler.go
+++ handle/artistsHandler.go
@@ -7,8 +7,6 @@ import (
 )
 
 func artistsHandler(w http.ResponseWriter, r *http.Request) {
-	// Check for filters
-
 	// Write the artists to the response
 	err := json.NewEncoder(w).Encode(data.Artists)
 	if err != nil {
